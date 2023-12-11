43a66cd7da489a83fa8c7d962704a94662d695f7
Nandana-Mohan <115038219+Nandana-Mohan@users.noreply.github.com>
2023-12-12 04:09:47 +0530

    Update db-connection.php

--- db-connection.php
+++ db-connection.php
@@ -1,12 +1,12 @@
 <?php
 
-$servername = "trmsql.mysql.database.azure.com";
+$servername = "trmsserver1.mysql.database.azure.com";
 $username = "nandana";
 $password = "March14032003";
 $databasename = "trms";
-$port = 3306;
 
-$conn = mysqli_connect($servername, $username, $password, $databasename, $port);
+
+$conn = mysqli_connect($servername, $username, $password, $databasename);
 
 if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL: ".mysqli_connect_errno();
